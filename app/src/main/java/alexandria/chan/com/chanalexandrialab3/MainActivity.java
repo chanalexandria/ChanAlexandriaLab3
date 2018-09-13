a467363399d7d396a092fa18a4e0735cd74b6355
chanalexandria <2015082635@ust-ics.mygbiz.com>
2018-09-13 22:13:46 +0800

    Initial commit

---
+++ app/src/main/java/alexandria/chan/com/chanalexandrialab3/MainActivity.java
@@ -0,0 +1,40 @@
+package alexandria.chan.com.chanalexandrialab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab3);
+
+        int images[] = {R.drawable.slide1, R.drawable.slide3, R.drawable.slide4};
+
+    v_flipper = findViewById(R.id.v_flipper);
+
+    for(int image:images)
+    {
+        flipperImages(image);
+    }
+
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(3000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+}
